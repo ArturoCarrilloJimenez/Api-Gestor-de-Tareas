58ede66d29708f04069873d12f6181e714656900
arturillo40 <arturojimenezalmontaras@gmail.com>
2024-11-08 11:47:35 +0000

    añado CORSMiddleware para poder tener orígenes cruzados

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.route import routes
 from crud.tareas_controller import routesCrudTask
@@ -8,6 +9,20 @@ import model.model as Models
 from configs.db import engine
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:4200",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Creación del modelo
 Models.Base.metadata.create_all(bind=engine)
